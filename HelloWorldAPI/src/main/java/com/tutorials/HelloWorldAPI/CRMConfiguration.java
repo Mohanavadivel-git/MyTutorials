72e662cbe02a12144fdca5fb520caa34e418e09b
VMOHANAV <vmohanav@ford.com>
2020-12-06 21:57:26 +0530

    [Add] Profile based Configurations

--- HelloWorldAPI/src/main/java/com/tutorials/HelloWorldAPI/CRMConfiguration.java
+++ HelloWorldAPI/src/main/java/com/tutorials/HelloWorldAPI/CRMConfiguration.java
@@ -2,12 +2,21 @@ package com.tutorials.HelloWorldAPI;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 
 @Configuration
 public class CRMConfiguration {
 
     @Bean
-    public CRMRepository getCRMRepository(){
+    @Profile("NA")
+    public CRMRepository getNACRMRepository(){
         return new NorthAmericaRepository();
+    }
+
+    @Bean
+    @Profile("China")
+    public CRMRepository getChinaCRMRepository(){
+        return new ChinaRepository();
     }
 }
